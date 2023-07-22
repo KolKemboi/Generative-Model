488333464c4cd780c4b63a7545c666fd50c99126
Kemboi Cheruiyot <87800637+KolKemboi@users.noreply.github.com>
2023-07-22 10:29:12 +0300

    reshaping the image to 28 by 28

--- emoji_gan/data prep.py
+++ emoji_gan/data prep.py
@@ -3,6 +3,17 @@ import os
 import shutil
 from time import sleep
 
-path = "C:\\Users\\USER\\Documents\\datasets\\emoji dataset"
+fold_path = "C:\\Users\\USER\\Documents\\datasets\\emoji dataset"
 
-files = os.listdir(path)
+files = os.listdir(fold_path)
+
+for file in files:
+	image = cv2.imread(os.path.join(fold_path, file))
+	gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	reshaped_image = cv2.resize(gray_image, (28, 28))
+	cv2.imshow("image", reshaped_image)
+	print(reshaped_image.shape)
+	cv2.waitKey(0)
+	
+
+cv2.destroyAllWindows()
